f8389d9d14bd19587271bfff404612b77eced239
boye <boye_1990@aliyun.com>
2020-12-24 18:14:04 +0800

    vue3 公共逻辑抽离 compostion Api的魅力

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/images/mapping.png">
-  <div class="count">{{ count }}</div>
-  <div class="count">{{ double }}</div>
-  <ul>
-    <li v-for="number in numbers" :key="number"> {{number}}</li>
-  </ul>
-  <div>{{person.name}}</div>
-  <div class="btn" @click="increase"> ++ </div>
+  <h1>监听鼠标点击坐标</h1>
+  <div>
+    <span>x:{{x}}</span>  | <span>y:{{y}}</span>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, computed, watch, reactive, toRefs, onMounted, onUpdated, onRenderTracked, onRenderTriggered } from 'vue';
-
-interface DataProps {
-  count: number;
-  double: number;
-  increase: () => void;
-  numbers: number[];
-  person: {name?: string};
-}
+import { defineComponent } from 'vue';
+import useMousePostion from './hooks/useMousePostion'
 
 export default defineComponent({
   name: 'App',
   setup() {
-    // // ref 创建的是响应式对象
-    // const count = ref(0)
-    // // computed 是计算熟悉，他的参数是一个函数
-    // const double = computed(() => {
-    //   return count.value * 2
-    // })
-    // // 普通函数
-    // const increase = () => {
-    //   count.value++
-    // }
-    onMounted(() => {
-      // console.log('onMounted')
-    })
-    onUpdated(() => {
-      // console.log('onUpdated')
-    })
-    onRenderTracked((e) => {
-      // console.log('onRenderTracked', e)
-    })
-    onRenderTriggered((e) => {
-      // console.log('onRenderTriggered', e)
-    })
+    const { x, y } = useMousePostion()
 
-    const title = ref('watch')
-
-    // reactive 生成响应式对象
-    const data: DataProps = reactive({
-      count: 0,
-      increase: () => { 
-        data.count++
-        data.numbers[0] = data.count
-        data.person.name = `张三${data.count}`
-        title.value = `${title.value}+${data.count}`
-        window.document.title = title.value
-       },
-      double: computed(() => data.count * 2),
-      numbers: [0, 1, 2, 3],
-      person: {}
-    })
-    watch([title, () => data.count], (newValue, old) => {
-      console.log(old, newValue)
-    })
-    // 如果直接展开data，他的每一项将失去响应式对象属性。因此要通过toRefs 转化为 响应式对象
-    const refData = toRefs(data)
     return {
-      ...refData
+      x,
+      y
     }
   },
 
